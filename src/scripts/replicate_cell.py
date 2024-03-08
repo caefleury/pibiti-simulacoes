3b2ed4dae512c4184314d9c385d7ad78fb8650d7
Caetano <caefleury15@gmail.com>
2024-03-08 13:38:16 -0300

    style: lint fix

--- src/scripts/replicate_cell.py
+++ src/scripts/replicate_cell.py
@@ -2,7 +2,7 @@
 # Vetores de rede (lattice constants)
 a = 6.3028
 b = 4.9302
-lattice_constants = [a,b]
+lattice_constants = [a, b]
 
 # Parâmetros
 unit_cell_file = 'src/simulations/unit_cell.xyz'
@@ -10,6 +10,8 @@ n_replications_x = 15
 n_replications_y = 15
 
 # Ler o aqruivo XYZ
+
+
 def read_xyz(file):
     with open(file, 'r') as f:
         n_atoms = int(f.readline())
@@ -21,16 +23,21 @@ def read_xyz(file):
     return n_atoms, comment, atoms
 
 # Replicar a célula unitária
+
+
 def replicate_cell(atoms, lattice_constants, n_replications_x, n_replications_y):
     replicated_atoms = []
     for i in range(n_replications_x):
         for j in range(n_replications_y):
             for atom, position in atoms:
-                new_position = [position[0] + (lattice_constants[0]) * i , position[1] + (lattice_constants[1])*j , 0.0]
+                new_position = [
+                    position[0] + (lattice_constants[0]) * i, position[1] + (lattice_constants[1])*j, 0.0]
                 replicated_atoms.append((atom, new_position))
     return replicated_atoms
 
 # Escrever o arquivo .xyz
+
+
 def escrever_xyz(file, n_atoms, comment, atoms):
     with open(file, 'w') as f:
         f.write(str(n_atoms) + '\n')
@@ -41,15 +48,19 @@ def escrever_xyz(file, n_atoms, comment, atoms):
             z = format(position[2], '.16f')
             f.write('{} {} {} {}\n'.format(atom, x, y, z))
 
+
 # Ler a célula unitária
 n_atoms, comment, atoms = read_xyz(unit_cell_file)
 
 # Replicar a célula unitária
-replicated_atoms = replicate_cell(atoms, lattice_constants, n_replications_x, n_replications_y)
+replicated_atoms = replicate_cell(
+    atoms, lattice_constants, n_replications_x, n_replications_y)
 
 # Escrever o arquivo .xyz com a estrutura replicada
 OUTPUT_STRUCTURE_FILE = 'src/simulations/structure.xyz'
 
-escrever_xyz(OUTPUT_STRUCTURE_FILE, n_replications_x * n_replications_y * n_atoms, comment, replicated_atoms)
+escrever_xyz(OUTPUT_STRUCTURE_FILE, n_replications_x *
+             n_replications_y * n_atoms, comment, replicated_atoms)
 
-print('Estrutura replicada com sucesso! arquivo salvo em {}'.format(OUTPUT_STRUCTURE_FILE))
+print('Estrutura replicada com sucesso! arquivo salvo em {}'.format(
+    OUTPUT_STRUCTURE_FILE))
--- tests/test.py
+++ tests/test.py
@@ -1,16 +1,16 @@
 import math
 
 hip = 1.42
-cateto = math.sqrt((hip**2) / 2) # 1.0023
+cateto = math.sqrt((hip**2) / 2)  # 1.0023
 
 a = 6.3028
 b = 4.9302
-x = (a - ((2*cateto) + (2*hip))) / 2 # 0.7238
-y = (b - (hip + (2*cateto))) / 2 # 0.75
+x = (a - ((2*cateto) + (2*hip))) / 2  # 0.7238
+y = (b - (hip + (2*cateto))) / 2  # 0.75
 
 x1 = [x+cateto,                  y,                 0.0]
 x2 = [x+cateto+hip,              y,                 0.0]
-x3 = [x+cateto+(2*hip),          y,                 0.0]       
+x3 = [x+cateto+(2*hip),          y,                 0.0]
 
 x4 = [x+(2*cateto)+(2*hip),      y+cateto,          0.0]
 x5 = [x+(2*cateto)+(2*hip),      y+cateto+hip,      0.0]
@@ -19,17 +19,19 @@ x6 = [x+cateto+(2*hip),      y+(2*cateto)+hip,      0.0]
 x7 = [x+cateto+hip,          y+(2*cateto)+hip,      0.0]
 x8 = [x+cateto,              y+(2*cateto)+hip,      0.0]
 
-x9 = [x,                     y+cateto+hip,          0.0] 
-x10= [x,                     y+cateto,              0.0] 
+x9 = [x,                     y+cateto+hip,          0.0]
+x10 = [x,                     y+cateto,              0.0]
+
 
 def padraoAtomo(atomo):
-    x,y,z = atomo
+    x, y, z = atomo
     x = format(x, '.16f')
     y = format(y, '.16f')
     z = format(z, '.16f')
     return f"C       {x}       {y}       {z}"
 
-lista = [x1,x2,x3,x4,x5,x6,x7,x8,x9,x10]
+
+lista = [x1, x2, x3, x4, x5, x6, x7, x8, x9, x10]
 listaPadrao = [padraoAtomo(atomo) for atomo in lista]
 
 with open('tests/test.xyz', 'w') as file:
