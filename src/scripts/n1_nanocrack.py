baa5a900ace5ada1e019792dc317eaa53ba7c557
Caetano <caefleury15@gmail.com>
2024-03-11 18:46:44 -0300

    feat: initial nanocrack generic case config

--- src/scripts/n1_nanocrack.py
+++ src/scripts/n1_nanocrack.py
@@ -1,17 +1,4 @@
-
-# Vetores de rede (lattice constants)
-a = 6.3028
-b = 4.9302
-lattice_constants = [a, b]
-
-# Parâmetros
-unit_cell_file = 'src/simulations/unit_cell.xyz'
-n_replications_x = 15
-n_replications_y = 15
-
 # Ler o arquivo XYZ
-
-
 def read_xyz(file):
     with open(file, 'r') as f:
         n_atoms = int(f.readline())
@@ -23,22 +10,26 @@ def read_xyz(file):
     return n_atoms, comment, atoms
 
 # Replicar a célula unitária com os nanocracks lineares (n1)
-
-
 def replicate_cell(atoms, lattice_constants, n_replications_x, n_replications_y):
     replicated_atoms = []
     for i in range(n_replications_x):
         for j in range(n_replications_y):
-            for atom, position in atoms:
-                new_position = [
-                    position[0] + (lattice_constants[0]) * i, position[1] + (lattice_constants[1])*j, 0.0]
+            for index,(atom, position) in enumerate(atoms):
+                if i == 7:
+                    atom = 'H'
+                    if j < 7:
+                        if j == 0:
+                            new_position = [position[0] + (lattice_constants[0]) * i, position[1] + (lattice_constants[1])*j, 0.0]
+                else:
+                    new_position = [
+                        position[0] + (lattice_constants[0]) * i, position[1] + (lattice_constants[1])*j, 0.0]
                 replicated_atoms.append((atom, new_position))
+
+            
     return replicated_atoms
 
 # Escrever o arquivo .xyz
-
-
-def escrever_xyz(file, n_atoms, comment, atoms):
+def write_xyz(file, n_atoms, comment, atoms):
     with open(file, 'w') as f:
         f.write(str(n_atoms) + '\n')
         f.write(comment + '\n')
@@ -47,6 +38,17 @@ def escrever_xyz(file, n_atoms, comment, atoms):
             y = format(position[1], '.16f')
             z = format(position[2], '.16f')
             f.write('{} {} {} {}\n'.format(atom, x, y, z))
+
+
+# Vetores de rede (lattice constants)
+a = 6.3028
+b = 4.9302
+lattice_constants = [a, b]
+
+# Parâmetros
+unit_cell_file = 'src/simulations/unit_cell.xyz'
+n_replications_x = 15
+n_replications_y = 15
 
 
 # Ler a célula unitária
@@ -59,7 +61,7 @@ replicated_atoms = replicate_cell(
 # Escrever o arquivo .xyz com a estrutura replicada
 OUTPUT_STRUCTURE_FILE = 'src/simulations/n1_nanocrack_structure.xyz'
 
-escrever_xyz(OUTPUT_STRUCTURE_FILE, n_replications_x *
+write_xyz(OUTPUT_STRUCTURE_FILE, n_replications_x *
              n_replications_y * n_atoms, comment, replicated_atoms)
 
 print('Estrutura replicada com sucesso! arquivo salvo em {}'.format(
